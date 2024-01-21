087822518134babc989a77878e846959a52448e2
mark <mark20306@yahoo.com.tw>
2024-01-21 17:43:07 +0800

    end of 9

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,47 @@
+import { Button, Icon, Item, ItemGroup, Segment, SegmentGroup } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { Link } from "react-router-dom";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityListItem({activity}: Props){
+    
+    return(
+        <SegmentGroup>
+            <Segment>
+                <ItemGroup>
+                    <Item>
+                        <Item.Image size="tiny" circular src= '/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Host by Mark</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </ItemGroup>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name="clock" /> {activity.date}
+                    <Icon name="marker" /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color="teal"
+                    floated="right"
+                    content="View"
+                />
+            </Segment>
+        </SegmentGroup>
+    )
+}
